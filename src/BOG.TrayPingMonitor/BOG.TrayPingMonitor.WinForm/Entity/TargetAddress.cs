c371ac2261700e5e606b63e9478c08f552c9b092
John J Schultz <rambotech@users.noreply.github.com>
2025-07-13 15:22:15 -0400

    base project (wip)

---
+++ src/BOG.TrayPingMonitor/BOG.TrayPingMonitor.WinForm/Entity/TargetAddress.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BOG.TrayPingMonitor.WinForm.Entity
+{
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class TargetAddress
+    {
+        [JsonProperty]
+        public string IpAddress { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public TimeSpan PingNormalFrequency { get; set; } = TimeSpan.FromSeconds(30);
+
+        public TimeSpan PingFocusedFrequency { get; set; } = TimeSpan.FromSeconds(5);
+
+        public int MissedPingWarningCount { get; set; } = 2;
+
+        public int MissedPingErrorCount { get; set; } = 4;
+
+        public int PingUnrespondedCount { get; set; } = 0;
+
+        public int PingRecoveryCount { get; set; } = 5;
+
+        public DateTime PingUnrespondedFirstTime { get; set; } = DateTime.MinValue;
+
+        public DateTime PingUnrespondedRecentTime { get; set; } = DateTime.MinValue;
+
+        public DateTime NotificationsStfuUntil { get; set; } = DateTime.MinValue;
+
+        public DateTime LastActivity { get; set} = DateTime.MinValue;
+    }
+}
---
+++ src/BOG.TrayPingMonitor/BOG.TrayPingMonitor.WinForm/TrayForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace BOG.TrayPingMonitor.WinForm
+{
+    partial class TrayForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrayForm));
+            btnOK = new Button();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            toolStripMenuItem1 = new ToolStripSeparator();
+            notifyIcon1 = new NotifyIcon(components);
+            tmrFormClose = new System.Windows.Forms.Timer(components);
+            tmrPingLaunch = new System.Windows.Forms.Timer(components);
+            tmrIconUpdate = new System.Windows.Forms.Timer(components);
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnOK
+            // 
+            btnOK.Location = new Point(353, 514);
+            btnOK.Margin = new Padding(4, 3, 4, 3);
+            btnOK.Name = "btnOK";
+            btnOK.Size = new Size(161, 27);
+            btnOK.TabIndex = 0;
+            btnOK.Text = "&Close Form/App";
+            btnOK.UseVisualStyleBackColor = true;
+            btnOK.Click += btnOK_Click;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { toolStripMenuItem1 });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 10);
+            contextMenuStrip1.ItemClicked += contextMenuStrip1_ItemClicked;
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(57, 6);
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "WinFormWithTrayIcon";
+            notifyIcon1.Visible = true;
+            notifyIcon1.DoubleClick += notifyIcon1_DoubleClick;
+            // 
+            // tmrFormClose
+            // 
+            tmrFormClose.Tick += timer1_Tick;
+            // 
+            // TrayForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(799, 589);
+            Controls.Add(btnOK);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "TrayForm";
+            Text = "Ping Activity";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load;
+            Resize += Form1_Resize;
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer tmrFormClose;
+        private System.Windows.Forms.Timer tmrPingLaunch;
+        private System.Windows.Forms.Timer tmrIconUpdate;
+    }
+}
